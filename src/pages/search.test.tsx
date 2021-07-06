a959cd6a757ecbec0dbf90addf3d6ab4e858b9ce
Tolu Adesegha <tolu@adesegha.ca>
2021-07-06 17:45:34 -0600

    add more tests around search page functionality

--- src/pages/search.test.tsx
+++ src/pages/search.test.tsx
@@ -3,11 +3,12 @@ import { render, screen, waitFor } from "@testing-library/react";
 import user from "@testing-library/user-event";
 import { performSearch } from "lib/search";
 import searchResult from "../../data/searchResult";
+import { SearchResult } from "domain/searchResults";
 
 jest.mock("lib/search");
 
-test("search performs search and displays results", async () => {
-  performSearch.mockResolvedValue({ success: true, ...searchResult });
+const init = (searchResponse: SearchResult) => {
+  performSearch.mockResolvedValue(searchResponse);
 
   render(<SearchPage />);
 
@@ -17,9 +18,54 @@ test("search performs search and displays results", async () => {
 
   user.type(searchBox, query);
   user.click(searchButton);
+};
+
+test("performs search and displays results", async () => {
+  const searchResponse = { success: true, ...searchResult };
+  init(searchResponse);
 
   await waitFor(() => {
     const searchResults = screen.getByText("Name: ICE CREAM, Alden's, Vanilla Bean");
     expect(searchResults).toBeInTheDocument();
+  });
+});
+
+test("performs search and displays no results message when search fails", async () => {
+  const searchResponse = { success: false } as SearchResult;
+  init(searchResponse);
+
+  await waitFor(() => {
+    const searchResults = screen.getByText(/Darn/);
+    expect(searchResults).toBeInTheDocument();
+  });
+});
+
+test("performs search and displays no results message when no results", async () => {
+  const searchResponse = { success: true, ...searchResult, products: [] };
+  init(searchResponse);
+
+  await waitFor(() => {
+    const searchResults = screen.getByText(/Darn/);
+    expect(searchResults).toBeInTheDocument();
+  });
+});
+
+test("displays pagination when more than one page of results is returned", async () => {
+  const searchResponse = { success: true, ...searchResult, numPages: 2 };
+  init(searchResponse);
+
+  await waitFor(() => {
+    const searchResults = screen.getByText("1");
+    expect(searchResults).toBeInTheDocument();
+  });
+});
+
+test("does not display pagination when only one page of results is returned", async () => {
+  const searchResponse = { success: true, ...searchResult, numPages: 1 };
+  init(searchResponse);
+
+  await waitFor(() => {
+    const searchResults = screen.queryByText("1");
+    expect(searchResults).not.toBeInTheDocument();
   });
 });
